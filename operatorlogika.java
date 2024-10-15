6e6a0027e05bc5133ca0f084523ce9d8ec84dce0
mawarkanu <mawarkanu08@gmail.com>
2024-10-15 10:01:39 +0800

    Add files via upload

---
+++ operatorlogika.java
@@ -0,0 +1,14 @@
+public class operatorlogika {
+	public static void main(String[] args) {
+		boolean x = true;
+		boolean y = false;
+
+		// Operator logika AND (&&)
+		boolean result = x && y;  // Hasilnya false
+		System.out.println("Hasil x && y = " + result);
+
+		// Operator logika OR (||)
+		result = x || y;  // Hasilnya true
+		System.out.println("Hasil x || y = " + result);
+	}
+}
---
+++ operatorpenugasan.java
@@ -0,0 +1,13 @@
+public class operatorpenugasan {
+	public static void main(String[] args) {
+		int c = 10;
+
+		// Operator penugasan penambahan (+=)
+		c += 5;  // Sama dengan c = c + 5;
+		System.out.println("Hasil c += 5 = " + c);
+
+		// Operator penugasan pengurangan (-=)
+		c -= 3;  // Sama dengan c = c - 3;
+		System.out.println("Hasil c -= 3 = " + c);
+	}
+}
